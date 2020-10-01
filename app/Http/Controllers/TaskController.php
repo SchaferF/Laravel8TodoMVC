4e4a3ad1157d82d5f9c42cda848dac486894104b
SchaferF <florian.schafer@wanadoo.fr>
2020-10-01 19:31:09 +0000

    Update taskcontroller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,32 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-
-    private $tasks;
-
-    public function __construct()
+    public function index ()
     {
-        $this->tasks = collect([
-            ['id' => 2, 'name' => 'Learn laravel', 'duration' => 12],
-            ['id' => 3, 'name' => 'Learn RubyOnRails', 'duration' => 24]
-        ])->keyBy('id');
-    }
-
-    //Action
-    public function index()
-    {
-        //DB Access
-        return view('task.index')->with('tasks', $this->tasks);
+        $tasks = Task::all();
+        return view('task.index')->with('tasks', $tasks);
     }
 
     public function show( $task )
     {
-        //DB access, to retrieve task with ID $task
-        return view('task.show')->with('task', $this->tasks[$task]);
+        $task = Task::find($task);
+        return view('task.show')->with('task', $task);
     }
 
+    public function create( )
+    {
+        return view('task.create');
+    }
+
+    public function store( Request $request )
+    {
+        $task = Task::create($request->all());
+        return view('task.show')->with('task', $task);
+    }
+
+    public function destroy($task)
+    {
+        $task = Task::find($task);
+        $task->delete();
+        return redirect(route('tasks.index'));
+    }
 }

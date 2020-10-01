ec2b495d139fd8878b1e63d80eb1e22e06e514c1
SchaferF <florian.schafer@wanadoo.fr>
2020-10-01 19:21:34 +0000

    Update the show view

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -1,8 +1,11 @@
 @extends('layout')
 
 @section('content')
-<h1>Task #{{ $task.id }} detail</h1>
 
-<p><b>Name : </b>{{ $task.name }}</p>
+<h1>Detail of task #{{ $task->id }}</h1>
+
+<p><b>Name : </b>{{ $task->name }}</h1>
+
+<p><a href="{{ route('tasks.index') }}">Back to list</a></p>
 
 @endsection

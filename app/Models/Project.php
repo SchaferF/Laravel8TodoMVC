44e2224b76cc80f8d2fd86183de77c27e44cbc5b
SchaferF <florian.schafer@wanadoo.fr>
2020-10-01 17:20:46 +0000

    Add a function to load tasks and eager loading

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    //eager loading
+    protected $with = ['tasks'];
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Models\Task');
+    }
 }

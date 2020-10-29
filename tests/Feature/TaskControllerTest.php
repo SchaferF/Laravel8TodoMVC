86d6312416c4b0f8d7c73aa0a03fdd25c6f9ea4e
SchaferF <florian.schafer@wanadoo.fr>
2020-10-29 20:31:17 +0000

    Working on Tests

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskControllerTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function testIndex()
     {
@@ -15,5 +17,36 @@ class TaskControllerTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertViewHas('tasks');
+    }
+
+
+    public function testShow()
+    {
+        $task = Task::factory()->make();
+
+        $response = $this->get('/tasks', [$task->id]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testUnexistingTask()
+    {
+        $response = $this->get('/task/1445');
+
+        $response->assertStatus(404);
+    }
+
+    public function testCreateNewTaskWithName()
+    {
+        $response = $this->post('/create', ['name' => 'new Task']);
+
+        $response->assertStatus(200);
+    }
+
+    public function testCreateNewTaskWithoutName()
+    {
+        $response = $this->post('/create', ['name' => '']);
+
+        $response->assertStatus(404);
     }
 }
